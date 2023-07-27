18e72f76aee4236b843c2f1c6d649c13c76ad2b9
ExoticPyCoder <140758324+ExoticPyCoder@users.noreply.github.com>
2023-07-27 14:33:05 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 import streamlit as st
-streamlit.header("hello world")
-streamlit.text("apple")
-streamlit.text("banana")
-streamlit.text("orange")
+st.header("hello world")
+st.text("apple")
+st.text("banana")
+st.text("orange")
 st.button('Click here')
+st.radio('Pick one', ['cats', 'dogs'])
+st.color_picker('Pick a color')
