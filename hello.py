6b08ca627787487141bad0596cb2894a803e6e5c
ExoticPyCoder <140758324+ExoticPyCoder@users.noreply.github.com>
2023-07-27 14:42:51 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -8,4 +8,7 @@ st.radio('Pick one', ['cats', 'dogs'])
 st.color_picker('Pick a color')
 st.multiselect('Buy', ['milk', 'apples', 'potatoes'])
 st.slider('Pick a number', 0, 100)
-st.image('./header.png')
+with st.form(key='my_form'):
+username = st.text_input('Username')
+Password = st.text_input('Password')
+st.form_submit_button('Login')
