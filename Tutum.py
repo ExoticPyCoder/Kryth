868307e40311149d084433231441c9582a66c2e6
ExoticPyCoder <140758324+ExoticPyCoder@users.noreply.github.com>
2023-08-03 14:57:35 -0700

    Update Tutum.py

--- Tutum.py
+++ Tutum.py
@@ -2,5 +2,9 @@ import streamlit as st
 st.header("Tutum Hospitium")
 st.button('Charity Login')
 st.button('Donate')
-st.text("Tutum Hospitium ('Safe Shelter' in Latin) is a platform that connects donors with charities. Our vision is to maximize the effectiveness of donating supplies to charities using technology. We allow charities to post the list of supplies they need as wishlists, and enable donors to pick items from the list to donate. Learn more about us, and how you can get involved, by clicking below!")
+st.text("
+Tutum Hospitium ('Safe Shelter' in Latin) is a platform that connects donors with charities. Our vision is to maximize the effectiveness of donating supplies to
+charities using technology. We allow charities to post the list of supplies they need as wishlists, and enable donors to pick items from the list to donate. 
+Learn more about us and how you can get involved, by clicking below!
+")
 st.button('Start Now')
