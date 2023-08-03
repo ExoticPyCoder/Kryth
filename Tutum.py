54ac147d33a32da2f10de04e7c9bfd7a36103dbd
ExoticPyCoder <140758324+ExoticPyCoder@users.noreply.github.com>
2023-08-03 16:05:09 -0700

    Update Tutum.py

--- Tutum.py
+++ Tutum.py
@@ -18,13 +18,12 @@ com.html(
 <body>
 <h1 style="font-size: 50px;" class="important">Tutum Hospitium</h1>
 <h1 class="important">Welcome</h1>
-<big>
-<p class="important">Tutum Hospitium ('Safe Shelter' in Latin) is a platform that connects donors with charities. 
+<pre>
+Tutum Hospitium ('Safe Shelter' in Latin) is a platform that connects donors with charities. 
 Our vision is to maximize the effectiveness of donating supplies to charities using technology. 
 We allow charities to post the list of supplies they need as wishlists, and enable donors to pick items from the list to donate. 
 Learn more about us, and how you can get involved,by clicking below!
-</p>
-</big>
+</pre>
 </body>
 </html>
 """
